132c0d6078dc82ccd8b56357ef83fdbc9a5ac285
dbacsik <dbacsik@uw.edu>
2020-07-17 12:52:48 -0700

    Added flags to sbatch that direct cluster to use conda modules

--- run_Hutch_cluster.bash
+++ run_Hutch_cluster.bash
@@ -8,6 +8,9 @@ snakemake \
     --cluster-config cluster.yaml \
     --cluster "sbatch -c {cluster.cpus} -t {cluster.time} -J {cluster.name}" \
     --latency-wait 30 \
+    --use-envmodules \
+    --use-conda \
+    --conda-prefix ./env
     -R `snakemake --list-input-changes`  # https://snakemake.readthedocs.io/en/stable/project_info/faq.html#snakemake-does-not-trigger-re-runs-if-i-add-additional-input-files-what-can-i-do
 echo "Run of snakemake complete."
 
