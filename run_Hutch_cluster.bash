3168dcc3531c2ddacf9744db9e437b75968574a2
dbacsik <dbacsik@uw.edu>
2020-07-20 11:45:51 -0700

    Finally fixed cluster paramters.

--- run_Hutch_cluster.bash
+++ run_Hutch_cluster.bash
@@ -2,13 +2,16 @@
 
 set -e
 
+echo "Loading modules:"
+ml bcl2fastq2/2.20.0-foss-2018b
+echo "Modules loaded."
+
 echo "Running snakemake..."
 snakemake \
     -j 999 \
     --cluster-config cluster.yaml \
     --cluster "sbatch -c {cluster.cpus} -t {cluster.time} -J {cluster.name}" \
     --latency-wait 30 \
-    --use-envmodules \
     --use-conda \
     --conda-prefix ./env \
     -R `snakemake --list-input-changes`  # https://snakemake.readthedocs.io/en/stable/project_info/faq.html#snakemake-does-not-trigger-re-runs-if-i-add-additional-input-files-what-can-i-do
