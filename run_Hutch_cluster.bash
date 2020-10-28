2f744aa5b5791de43cb0bccc5f884bf88cda8c91
dbacsik <dbacsik@uw.edu>
2020-10-28 14:37:04 -0700

    Removed temporary until parameter

--- run_Hutch_cluster.bash
+++ run_Hutch_cluster.bash
@@ -10,7 +10,6 @@ snakemake \
     --cluster "sbatch -c {cluster.cpus} -t {cluster.time} -J {cluster.name}" \
     --latency-wait 60 \
     --use-conda \
-    --until viral_barcodes_in_progeny \
     -R `snakemake --list-input-changes`  # https://snakemake.readthedocs.io/en/stable/project_info/faq.html#snakemake-does-not-trigger-re-runs-if-i-add-additional-input-files-what-can-i-do
 echo "Run of snakemake complete."
 
